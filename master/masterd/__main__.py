a371e06b62e36d99e5825e5f6b6077eea3304fbb
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-12 18:49:11 +0100

    Call start from __main__

--- master/masterd/__main__.py
+++ master/masterd/__main__.py
@@ -7,9 +7,13 @@ import logging
 from version import __version__
 from update import update
 from log import setup_logging
+from daemon import start_daemon
 
 # setup logging
 logger = setup_logging()
 
 # check for updates and update
 updateStatus = update(__version__)
+
+# start daemon in background
+start_daemon())
