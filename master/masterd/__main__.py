55eaf8e260335b55e32da8da939f666d3a5cc1d5
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-12 13:26:17 +0100

    Added update functions

--- master/masterd/__main__.py
+++ master/masterd/__main__.py
@@ -1,3 +1,11 @@
 # dns-manager
 # __main__.py
 # first thing that starts, entry point (master)
+
+# import
+import logging
+from version import __version__
+from update import update
+
+# check for updates and update
+updateStatus = update() 
