0bccc91c86dc7d7cb0ec3298d76ef761c19230f2
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-12 17:25:59 +0100

    Improve logger & correct URL

--- master/masterd/__main__.py
+++ master/masterd/__main__.py
@@ -2,14 +2,17 @@
 # __main__.py
 # first thing that starts, entry point (master)
 
-# import
+# import (required header)
 import logging
 from version import __version__
 from update import update
 from log import setup_logging
 
 # setup logging
-setup_logging()
+logger = setup_logging()
 
 # check for updates and update
 updateStatus = update(__version__)
+
+logger.error("warning")
+logger.debug("just debug")
