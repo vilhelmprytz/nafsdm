5615da58f510d0816318073c35bf8268dbe27b23
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-12 17:30:58 +0100

    Inital file

---
+++ master/masterd/daemon.py
@@ -0,0 +1,11 @@
+# dns-manager
+# daemon.py
+# handles daemon functions
+
+# required header
+import logging
+import urllib
+from log import setup_logging
+
+# setup logging
+logger = setup_logging()
---
+++ master/masterd/database.py
@@ -0,0 +1,11 @@
+# dns-manager
+# database.py
+# handles database connections
+
+# required header
+import logging
+import urllib
+from log import setup_logging
+
+# setup logging
+logger = setup_logging()
