9f59a7b00679c1554ad840d1719718e3e88bf3b8
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-12 17:11:20 +0100

    New logging method

--- master/masterd/log.py
+++ master/masterd/log.py
@@ -3,6 +3,14 @@
 # sets up log stuf
 
 import logging
+import sys
 
 def setup_logging():
-    logging.basicConfig(filename="/etc/dns-manager/logs/master.log",level=logging.DEBUG)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    logco = logging.StreamHandler(sys.stdout)
+    logco.setLevel(logging.DEBUG)
+    
+    logger.addHandler(logco)
+    #logging.basicConfig(filename="/etc/dns-manager/logs/master.log",level=logging.DEBUG)
