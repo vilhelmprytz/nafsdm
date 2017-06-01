c9f0c1b5c2c1f36a57800ab0ea5d6f1158c91583
BuildTools <vpytkanal.sverige@gmail.com>
2017-06-01 21:22:04 +0200

    Inital setup scripts

---
+++ setupMaster.sh
@@ -0,0 +1,8 @@
+# nafsdm
+# help script for setting up nafsdm on the master
+# Copyright Vilhelm Prytz 2017
+
+GITHUB_URL="https://github.com/MrKaKisen/nafsdm.git"
+GITHUB_DIR="master-nafsdm"
+HOME_DIR="/home/master-nafsdm"
+USER="master-nafsdm"
---
+++ setupSlave.sh
@@ -0,0 +1,8 @@
+# nafsdm
+# help script for setting up nafsdm on all slaves
+# Copyright Vilhelm Prytz 2017
+
+GITHUB_URL="https://github.com/MrKaKisen/nafsdm.git"
+GITHUB_DIR="slave-nafsdm"
+HOME_DIR="/home/slave-nafsdm"
+USER="slave-nafsdm"
