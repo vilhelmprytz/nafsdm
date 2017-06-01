95ddbea6c706f210ddb12dbdda9094381be0dcea
BuildTools <vpytkanal.sverige@gmail.com>
2017-06-01 19:39:55 +0200

    Remove SSH key pass & update exit code

--- master/masterd/home/master-dnsman/pythondaemon/setup.py
+++ master/masterd/home/master-dnsman/pythondaemon/setup.py
@@ -6,6 +6,7 @@
 import subprocess
 import os, random, string
 
+# unused function, but will leave in here if it will be needed
 def generatePassword(length=30):
     chars = string.ascii_letters + string.digits + '!@#$%^&*()'
     random.seed = (os.urandom(1024))
@@ -13,13 +14,13 @@ def generatePassword(length=30):
     return(''.join(random.choice(chars) for i in range(length)))
 
 def setupSSH():
-    generatedPassword = generatePassword()
+    #generatedPassword = generatePassword()
     try:
         output = subprocess.check_output(["mkdir", "/home/master-dnsman/.ssh"])
-        output = subprocess.check_output(["ssh-keygen ", "-t", "rsa", "-b", "4096", "-C", "'DNS manager'", "-P", "'" + generatedPassword + "'", "-f", "'/home/master-dnsman/.ssh/dns_manager_rsa'", "-q"])
+        output = subprocess.check_output(["ssh-keygen ", "-t", "rsa", "-b", "4096", "-C", "'DNS manager'", "-P", "-f", "'/home/master-dnsman/.ssh/dns_manager_rsa'", "-q"])
         output = subprocess.check_output(["cp", "/home/master-dnsman/.ssh/dns_manager_rsa.pub", "/home/master-dnsman/.ssh/authorized_keys"])
     except Exception, e:
         log("FATAL: Some error ocurred during SSH key generation.")
 
     log("To continue, please copy /home/master-dnsman/dns_manager_rsa to all slaves /home/slave-dnsman/.ssh/master_key")
-    exit(0)
+    exit(1)
