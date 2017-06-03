52c8bf51488cf64e18ac0e3e75d3ce5ac4c9c264
BuildTools <vpytkanal.sverige@gmail.com>
2017-06-03 22:43:57 +0200

    NOT TESTED: Created setup script for master

--- setupMaster.sh
+++ setupMaster.sh
@@ -2,7 +2,58 @@
 # help script for setting up nafsdm on the master
 # Copyright Vilhelm Prytz 2017
 
+# check if user is root or not
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
 GITHUB_URL="https://github.com/MrKaKisen/nafsdm.git"
 GITHUB_DIR="master-nafsdm"
 HOME_DIR="/home/master-nafsdm"
 USER="master-nafsdm"
+
+echo "###################################################################"
+echo "THIS SCRIPT WILL NOT WORK FOR UPDATING YOUR INSTALLATION"
+echo "Welcome to nafsdm master install! Please enter your package manager (apt or yum only supported)."
+echo -n "Package manager > "
+read PACKAGEMAN
+
+if [ "$PACKAGEMAN" == "yum" ]; then
+  echo "Installing packages.. (NOTE: ROOT PASSW)"
+  yum update -y
+  yum install python python-pip git -y
+
+  pip install requests
+elif [ "$PACKAGEMAN" == "apt" ]; then
+  echo "Installing packages.."
+  apt-get update -y
+  apt-get install python python-pip git -y
+
+  pip install requests
+else
+  echo "Invalid package manager. Only apt and yum supported."
+  exit 1
+fi
+
+echo "Required packages installed!"
+echo "Downloading nafsdm & installing.."
+
+# download in temp dir
+cd /tmp
+git clone $GITHUB_URL
+
+useradd $USER
+cp /tmp/nafsdm/$GITHUB_DIR $HOME_DIR -R
+
+cp /tmp/nafsdm/systemconfigs/nafsdmctl /usr/bin/nafsdmctl
+cp /tmp/nafsdm/systemconfigs/nafsdm-master /usr/bin/nafsdm-master
+
+chmod +x /usr/bin/nafsdmctl
+chmod +x /usr/bin/nafsdm-master
+
+echo "Installed. Cleanup.."
+
+rm /tmp/nafsdm -rf
+
+echo "To continue, please run 'nafsdm-master' for first time setup."
