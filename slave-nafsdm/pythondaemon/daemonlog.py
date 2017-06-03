384a75f0fc4c22dc6b8172d99c92b23fb9bc06fa
BuildTools <vpytkanal.sverige@gmail.com>
2017-06-03 21:48:09 +0200

    Add print log for systemd & fix start file

--- slave-nafsdm/pythondaemon/daemonlog.py
+++ slave-nafsdm/pythondaemon/daemonlog.py
@@ -5,8 +5,12 @@
 from time import strftime
 
 def log(log):
+    # write it to a file with current time / date
     write = strftime("%a, %d %b %Y %H:%M:%S") + " - " + log + "\n"
 
     f = open("/home/slave-nafsdm/log.log", "a")
     f.write(write)
     f.close()
+
+    # as systemd is used, we also print it (but without time/date)
+    print(log)
