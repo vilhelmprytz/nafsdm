d6839311acfa967de837d53d30506f470ddd35c1
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2018-04-18 14:57:36 +0200

    temp change branch to development instead of master

--- scripts/upgradeSlave.sh
+++ scripts/upgradeSlave.sh
@@ -23,7 +23,7 @@ else
   exit 1
 fi
 
-BRANCH="master"
+BRANCH="development"
 DL_URL="https://github.com/MrKaKisen/nafsdm/archive/"
 MY_VERSION_RAW="`cat /home/slave-nafsdm/pythondaemon/version.py`"
 LATEST_VERSION=$(curl https://raw.githubusercontent.com/MrKaKisen/nafsdm/$BRANCH/version.txt)
