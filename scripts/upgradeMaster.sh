33ee24e283a234c2b3964f467523bc20e36485a7
Vilhelm Prytz <vilhelm@prytznet.se>
2018-05-27 21:25:10 +0200

    Webinterface restart on master upgrade if enabled and active

--- scripts/upgradeMaster.sh
+++ scripts/upgradeMaster.sh
@@ -435,6 +435,10 @@ else
   exit 1
 fi
 
+# restart webinterface if it exists, and is active
+echo "* Restart webinterface if it is enabled and if it is active."
+[ -f /etc/systemd/system/nafsdm-webinterface.service ] && systemctl is-active --quiet nafsdm-webinterface && systemctl restart nafsdm-webinterface
+
 DATE="`date`"
 echo "* Finished @ $DATE"
 
