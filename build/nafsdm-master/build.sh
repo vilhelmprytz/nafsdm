1110f057df86c6f89d14b9f9c4e3cd414d8edf45
Vilhelm Prytz <vilhelm@prytznet.se>
2018-08-25 19:50:33 +0200

    Update deb build script

--- build/nafsdm-master/build.sh
+++ build/nafsdm-master/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-$BUILD_PATH="build/nafsdm-master"
+BUILD_PATH="build/nafsdm-master"
 
 cd ../..
 
 mkdir $BUILD_PATH/home
 mkdir -p $BUILD_PATH/usr/bin
 
-cp -R nafsdm-master $BUILD_PATH/home
+cp -R master-nafsdm $BUILD_PATH/home
 cp systemconfigs/nafsdm-manager $BUILD_PATH/usr/bin/
 cp systemconfigs/nafsdmctl $BUILD_PATH/usr/bin
 cp systemconfigs/nafsdm-webinterface.service $BUILD_PATH/home/master-nafsdm/webinterface/nafsdm-webinterface.service
@@ -17,3 +17,9 @@ cp CHANGELOG.md $BUILD_PATH/home/master-nafsdm/changelog.txt
 cd build
 
 /usr/bin/env dpkg-deb --build nafsdm-master
+
+# cleanup
+cd nafsdm-master
+rm -rf home usr
+
+cd ..
